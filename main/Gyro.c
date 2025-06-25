bfe48f81aaa99d6a74ec70f92972c11660ff5e99
RevK <github@k.gg>
2025-06-25 10:14:37 +0100

    Library update

--- main/Gyro.c
+++ main/Gyro.c
@@ -1,2 +1,26 @@
 // Gyro app
 
+#include <revk.h>
+
+const char *
+app_callback (int client, const char *prefix, const char *target, const char *suffix, jo_t j)
+{
+	return NULL;
+}
+
+void
+revk_state_extra (jo_t j)
+{
+}
+
+void
+revk_web_extra (httpd_req_t * req, int page)
+{
+}
+
+void
+app_main ()
+{
+	revk_boot (&app_callback);
+	revk_start();
+}
