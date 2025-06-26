1c13eff6fa098378d66e9c4a615b300251eae496
RevK <github@k.gg>
2025-06-26 09:31:48 +0100

    Basic working

--- main/Gyro.c
+++ main/Gyro.c
@@ -36,7 +36,7 @@ revk_state_extra (jo_t j)
 
 }
 
-void
+esp_err_t
 i2c_write (uint8_t reg, uint8_t val)
 {
    i2c_cmd_handle_t t = i2c_cmd_link_create ();
@@ -45,26 +45,30 @@ i2c_write (uint8_t reg, uint8_t val)
    i2c_master_write_byte (t, reg, true);
    i2c_master_write_byte (t, val, true);
    i2c_master_stop (t);
-   i2c_master_cmd_begin (i2cport, t, 10 / portTICK_PERIOD_MS);
+   esp_err_t e = i2c_master_cmd_begin (i2cport, t, 100 / portTICK_PERIOD_MS);
    i2c_cmd_link_delete (t);
+   if (e)
+      ESP_LOGE (TAG, "I2C write fail %02X %02X", reg, val);
+   usleep (100000);
+   return e;
 }
 
-uint8_t
-i2c_read (uint8_t reg)
+esp_err_t
+i2c_read (uint8_t reg, uint8_t len, uint8_t * data)
 {
-   uint8_t v = 0;
    i2c_cmd_handle_t t = i2c_cmd_link_create ();
    i2c_master_start (t);
    i2c_master_write_byte (t, (addr << 1) | I2C_MASTER_WRITE, true);
    i2c_master_write_byte (t, reg, true);
    i2c_master_start (t);
    i2c_master_write_byte (t, (addr << 1) | I2C_MASTER_READ, true);
-   i2c_master_read_byte (t, &v, I2C_MASTER_LAST_NACK);
+   i2c_master_read (t, data, len, I2C_MASTER_LAST_NACK);
    i2c_master_stop (t);
-   if (i2c_master_cmd_begin (i2cport, t, 10 / portTICK_PERIOD_MS))
-      v = 0xFF;
+   esp_err_t e = i2c_master_cmd_begin (i2cport, t, 100 / portTICK_PERIOD_MS);
    i2c_cmd_link_delete (t);
-   return v;
+   if (e)
+      ESP_LOGE (TAG, "I2C read fail %02X", reg);
+   return e;
 }
 
 void
@@ -81,33 +85,50 @@ i2c_task (void *p)
    };
    i2c_param_config (i2cport, &config);
    i2c_set_timeout (i2cport, 31);
-   if (i2c_read (0x6B) != 0x40)
+   uint8_t id = 0;
+   if (i2c_read (0x75, 1, &id) || id != 0x68)
    {
-      ESP_LOGE (TAG, "I2C failed");
+      ESP_LOGE (TAG, "I2C failed %02X", id);
       vTaskDelete (NULL);
       return;
    }
+   i2c_write (0x6B, 0x80);      // Reset
+   i2c_write (0x6B, 0x08 + 5);  // No temp, clock 1
+   i2c_write (0x19, 100 - 1);   // Sample rate divide
+   i2c_write (0x1A, 0x01);      // Filter
    i2c_write (0x1B, 0x18);      // ±2000°/s
    i2c_write (0x1C, 0x18);      // ±16g
-   i2c_write (0x6B, 0x20);      // Run
-   i2c_write (0x6C, 0x20);      // 5Hz
+   i2c_write (0x23, 0x78);      // Acc and gyro FIFO
+   i2c_write (0x6A, 0x44);      // FIFO enable / reset
+
    while (1)
    {
-      uint8_t buf[14];
-      i2c_cmd_handle_t t = i2c_cmd_link_create ();
-      i2c_master_start (t);
-      i2c_master_write_byte (t, (addr << 1) | I2C_MASTER_WRITE, true);
-      i2c_master_write_byte (t, 0x3B, true);
-      i2c_master_start (t);
-      i2c_master_write_byte (t, (addr << 1) | I2C_MASTER_READ, true);
-      i2c_master_read (t, buf, sizeof (buf), I2C_MASTER_LAST_NACK);
-      i2c_master_stop (t);
-      if (!i2c_master_cmd_begin (i2cport, t, 10 / portTICK_PERIOD_MS))
+      uint8_t buf[12];
+      if (!i2c_read (0x72, 2, buf))
       {
-         ESP_LOG_BUFFER_HEX_LEVEL (TAG, buf, sizeof (buf), ESP_LOG_ERROR);
+         uint16_t fifo = ((buf[0] << 8) | buf[1]);
+         while (fifo)
+         {
+            if (!i2c_read (0x74, sizeof (buf), buf))
+            {
+               int16_t ax,
+                 ay,
+                 az,
+                 gx,
+                 gy,
+                 gz;
+               ax = (buf[0] << 8) | buf[1];
+               ay = (buf[2] << 8) | buf[3];
+               az = (buf[4] << 8) | buf[5];
+               gx = (buf[6] << 8) | buf[7];
+               gy = (buf[8] << 8) | buf[9];
+               gz = (buf[10] << 8) | buf[11];
+               ESP_LOGE (TAG, "%6d %6d %6d %6d %6d %6d", ax, ay, az, gx, gy, gz);
+            }
+            fifo -= sizeof (buf);
+         }
       }
-      i2c_cmd_link_delete (t);
-      usleep (200000);
+      usleep (100000);
    }
 }
 
